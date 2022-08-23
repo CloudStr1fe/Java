e171a1692f3776e6a9fc4c2561092d5f18f79a30
Cloud <gabriel.sk5@hotmail.com>
2022-08-23 16:10:37 -0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,43 @@
-import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
         // fazer uma conexão HTTP e buscar os top 250 filmes
-        String url = "https://animechan.vercel.app/api/quotes";
+        String url = "";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(endereco).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        System.out.println(body);
 
-        // pegar só os dados que interessam (titulo, poster, rating)
+        // extrair só os dados que interessam (titulo, poster, rating)
+        var parser = new JsonParser();
+        List<Map<String, String>> ListaDeFilmes = parser.parse(body);
 
-        // exibir manipular os dados
+        // exibir e manipular os dados
+        for (Map<String,String> filme : ListaDeFilmes) {
 
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = titulo + ".png";
+
+            var geradora = new GeradoraDeFigurinhas();
+            geradora.cria(inputStream, nomeArquivo);
+
+            System.out.println(titulo);
+            System.out.println();
+
+        }
     }
 }
